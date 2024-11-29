53c05c0e3b847b0c52098bb45ccefa2e6e7a4cca
jiggdev <jiggdev@gmail.com>
2024-11-29 17:19:00 +0100

    .

--- src/components/PreguntasComponent.js
+++ src/components/PreguntasComponent.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 function PreguntasComponent({ preguntas, titulo }) {
-    const titulo = titulo.replace('•', '');
+    const tituloStripped = titulo.replace('•', '');
     const [preguntaActual, setPreguntaActual] = useState(null);
     const [seleccion, setSeleccion] = useState(null); // Índice de la opción seleccionada
     const [revelada, setRevelada] = useState(false); // Si la respuesta y feedback están revelados
@@ -28,7 +28,7 @@ function PreguntasComponent({ preguntas, titulo }) {
         <div style={{ margin: '20px', padding: '15px' }}>
             {preguntaActual && (
                 <div className="card mb-3" style={{ marginBottom: '30px', padding: '15px' }}>
-{titulo && <h3 style={{ marginBottom: '20px', color: '#00aaff' }}>{titulo}</h3>}
+{tituloStripped && <h3 style={{ marginBottom: '20px', color: '#00aaff' }}>{tituloStripped}</h3>}
                     <div className="card-body">
                         <h4 className="card-title" style={{ marginBottom: '20px' }}>
                             {preguntaActual.enunciado}
